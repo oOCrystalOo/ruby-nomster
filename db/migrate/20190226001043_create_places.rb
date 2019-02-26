6bb4bda0b3cdc94d3aed5a065a01ba1ffe5c2335
Crystal Ng <ng.crystal.wy@gmail.com>
2019-02-26 00:40:10 +0000

    Added places  migration

---
+++ db/migrate/20190226001043_create_places.rb
@@ -0,0 +1,10 @@
+class CreatePlaces < ActiveRecord::Migration[5.2]
+  def change
+    create_table :places do |t|
+      t.string :name
+      t.text :description
+      t.string :address
+      t.timestamps
+    end
+  end
+end
