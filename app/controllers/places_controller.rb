d853f48faba27d785c0dcc2579d8db334c9d33fd
Crystal Ng <ng.crystal.wy@gmail.com>
2019-02-27 00:44:22 +0000

    Added input form

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -7,6 +7,17 @@ class PlacesController < ApplicationController
   end
   
   def new
-    
+    @place = Place.new
+  end
+  
+  def create
+    Place.create(place_params)
+    redirect_to root_path
+  end
+  
+  private
+  
+  def place_params
+    params.require(:place).permit(:name, :description, :address)
   end
 end
