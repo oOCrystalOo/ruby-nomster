b051ebe886389af8f0c625468401df0b777371be
Crystal Ng <ng.crystal.wy@gmail.com>
2019-03-02 01:30:12 +0000

    Added CRUD to Places

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -17,6 +17,26 @@ class PlacesController < ApplicationController
     redirect_to root_path
   end
   
+  def show
+    @place = Place.find(params[:id])
+  end
+  
+  def edit
+    @place = Place.find(params[:id])
+  end
+  
+  def update
+    @place = Place.find(params[:id])
+    @place.update_attributes(place_params)
+    redirect_to root_path
+  end
+  
+  def destroy
+    @place = Place.find(params[:id])
+    @place.destroy
+    redirect_to root_path
+  end
+  
   private
   
   def place_params
