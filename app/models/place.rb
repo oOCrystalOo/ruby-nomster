268182dfaf52f929f92add03e1a3846c6cbe193b
Crystal Ng <ng.crystal.wy@gmail.com>
2019-03-03 03:59:21 +0000

    Added more validation. Added maps

--- app/models/place.rb
+++ app/models/place.rb
@@ -3,5 +3,7 @@ class Place < ApplicationRecord
   geocoded_by :address
   after_validation :geocode
   
-  validates :name, presence: true
+  validates :name, presence: true, length: { minimum: 3 }
+  validates :address, presence: true
+  validates :description, presence: true
 end
