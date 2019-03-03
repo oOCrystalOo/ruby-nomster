2d8fb2290571a8aec9069202b9d349cbc4eeec0b
Crystal Ng <ng.crystal.wy@gmail.com>
2019-03-03 03:28:03 +0000

    Fixed up nav bar. Added validation when updating or adding place

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -13,8 +13,13 @@ class PlacesController < ApplicationController
   end
   
   def create
-    current_user.places.create(place_params)
-    redirect_to root_path
+    @place = current_user.places.create(place_params)
+    if @place.valid?
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+    
   end
   
   def show
@@ -36,7 +41,11 @@ class PlacesController < ApplicationController
     
     @place = Place.find(params[:id])
     @place.update_attributes(place_params)
-    redirect_to root_path
+    if @place.valid?
+      redirect_to root_path
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
   
   def destroy
