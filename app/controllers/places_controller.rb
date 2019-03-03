7e4d690e21af376f7d12cc8ad599464332216ebb
Crystal Ng <ng.crystal.wy@gmail.com>
2019-03-03 18:22:58 +0000

    Fixed error in place#destroy

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -50,11 +50,11 @@ class PlacesController < ApplicationController
   end
   
   def destroy
+    @place = Place.find(params[:id])
     if @place.user != current_user
       return render plain: 'Not Allowed', status: :forbidden
     end
     
-    @place = Place.find(params[:id])
     @place.destroy
     redirect_to root_path
   end
