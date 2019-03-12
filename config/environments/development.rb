0675e90094643af42be46dfd366af3bd8c71d047
Crystal Ng <ng.crystal.wy@gmail.com>
2019-03-12 01:37:47 +0000

    Added mailer config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,17 @@
 Rails.application.configure do
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "example.com",
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: ENV['GMAIL_ADDRESS'],
+    password: ENV['GMAIL_PASSWORD']
+  }
+  # config.action_mailer.perform_deliveries = true
+  
   config.action_mailer.default_url_options = { host: 'localhost:3030' }
   
   # Settings specified here will take precedence over those in config/application.rb.
