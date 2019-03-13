f892694f864b4738c84cacc0de783439dcf27b63
Crystal Ng <ng.crystal.wy@gmail.com>
2019-03-13 00:33:03 +0000

    Revised email template.

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,8 +1,10 @@
 class NotificationMailer < ApplicationMailer
   default from: "no-reply@nomsterapp.com"
   
-  def comment_added
-    mail(to: "ng.crystal.wy@gmail.com",
-         subject: "A comment has been added to your place")
+  def comment_added(comment)
+    @place = comment.place
+    @place_owner = @place.user
+    mail(to: @place_owner.email,
+         subject: "A comment has been added to #{@place.name}")
   end
 end
