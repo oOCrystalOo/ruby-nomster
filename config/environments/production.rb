7bf0a26676a7e7bb2ef84a4feccf010df9cbc53b
Crystal Ng <ng.crystal.wy@gmail.com>
2019-03-14 22:50:09 +0000

    Added sendgrid config to production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,14 +1,15 @@
 Rails.application.configure do
-  config.action_mailer.delivery_method = :smtp
-
-  config.action_mailer.smtp_settings = {
-    address: "smtp.gmail.com",
-    port: 587,
-    authentication: "plain",
-    enable_starttls_auto: true,
-    user_name: ENV['GMAIL_ADDRESS'],
-    password: ENV['GMAIL_PASSWORD']
+  
+  ActionMailer::Base.smtp_settings = {
+    address: 'smtp.sendgrid.net',
+    port: '587',
+    authentication: :plain,
+    user_name: ENV['SENDGRID_USERNAME'],
+    password: ENV['SENDGRID_PASSWORD'],
+    domain: 'heroku.com',
+    enable_starttls_auto: true
   }
+
   config.action_mailer.default_url_options = { host: 'nomster-crystal-ng.herokuapp.com' }
   
   # Settings specified here will take precedence over those in config/application.rb.
